df1b6a04d3e90b93f18ba20cd19ad353135fcf72
Bogdan Klichuk <klichukb@gmail.com>
2015-12-23 21:44:31 +0200

    Test postgresql settings

--- test_project/test_app/settings.py
+++ test_project/test_app/settings.py
@@ -60,8 +60,12 @@ WSGI_APPLICATION = 'test_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'migrate_sql_test_db',
+        'USER': 'migrate_sql_test_db',
+        'PASSWORD': 'test',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
