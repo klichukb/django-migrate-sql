204ed0ac9a11cc61ac95f5dc788e963caa27bc93
Bogdan Klichuk <klichukb@gmail.com>
2015-12-23 21:35:24 +0200

    Richer sample models / initial migration

--- test_project/test_app/migrations/0001_initial.py
+++ test_project/test_app/migrations/0001_initial.py
@@ -11,14 +11,19 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='Author',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+                ('rating', models.IntegerField(null=True, blank=True)),
+            ],
+        ),
+        migrations.CreateModel(
             name='Book',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=200)),
-                ('author', models.CharField(max_length=200)),
+                ('author', models.ForeignKey(to='test_app.Author')),
             ],
-            options={
-            },
-            bases=(models.Model,),
         ),
     ]
--- test_project/test_app/models.py
+++ test_project/test_app/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    rating = models.IntegerField(null=True, blank=True)
 
 class Book(models.Model):
     name = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
+    author = models.ForeignKey(Author)
