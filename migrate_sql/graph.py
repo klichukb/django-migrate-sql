0ec8bd8471093f37dceb321e00a961c8aca2aabe
Bogdan Klichuk <klichukb@gmail.com>
2015-12-27 15:48:57 +0200

    Introduced graph for sql items.

---
+++ migrate_sql/graph.py
@@ -0,0 +1,46 @@
+from collections import namedtuple
+
+from django.db.migrations.graph import Node, NodeNotFoundError
+from django.apps import apps
+
+SqlItemNode = namedtuple('SqlItemNode', ('sql', 'reverse_sql'))
+
+
+class SqlStateGraph(object):
+    def __init__(self):
+        self.nodes = {}
+        self.node_map = {}
+
+    def add_node(self, key, sql_item):
+        node = Node(key)
+        self.node_map[key] = node
+        self.nodes[key] = sql_item
+
+    def add_dependency(self, app_label, child, parent):
+        if child not in self.nodes:
+            raise NodeNotFoundError(
+                "App %s dependencies reference nonexistent child node %r" % (app_label, child),
+                child
+            )
+        if parent not in self.nodes:
+            raise NodeNotFoundError(
+                "App %s dependencies reference nonexistent parent node %r" % (app_label, parent),
+                parent
+            )
+        self.node_map[child].add_parent(self.node_map[parent])
+        self.node_map[parent].add_child(self.node_map[child])
+        self.clear_cache()
+
+
+def build_current_graph():
+    graph = SqlStateGraph()
+    for config in apps.get_app_configs():
+        if hasattr(config, 'custom_sql'):
+            for sql_item in config.custom_sql:
+                graph.add_node(
+                    (config.label, sql_item.name),
+                    SqlItemNode(sql_item.sql, sql_item.reverse_sql),
+                )
+                for dep in sql_item.dependencies:
+                    graph.add_dependency(config.label, sql_item.name, dep)
+    return graph
