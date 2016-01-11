34c209cee9069d97d5b7f4afcbaa4c6aac40aadc
Bogdan Klichuk <klichukb@gmail.com>
2016-01-11 11:07:05 +0200

    Added test runner

---
+++ runtests.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.test_project.settings")
+sys.path.insert(0, 'tests')
+
+import django
+from django.core.management import call_command
+
+django.setup()
+
+call_command('test')
+
+sys.exit(0)
