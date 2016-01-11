2376723bee2ceec480d2f7aeea5234ce1e7bfac5
Bogdan Klichuk <klichukb@gmail.com>
2016-01-11 11:06:57 +0200

    Added requirements files

--- setup.py
+++ setup.py
@@ -3,11 +3,33 @@
 
 import re
 import os
+import sys
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+class Tox(TestCommand):
+    user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.tox_args = None
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import tox
+        import shlex
+
+        args = self.tox_args
+        if args:
+            args = shlex.split(self.tox_args)
+        errno = tox.cmdline(args=args)
+        sys.exit(errno)
 
 
 def get_version(package):
@@ -42,6 +64,7 @@ setup(
         "Programming Language :: Python :: 2",
         'Programming Language :: Python :: 2.7',
     ],
-    test_suite='runtests',
+    tests_require=['tox'],
+    cmdclass={'test': Tox},
     install_requires=[],
 )
